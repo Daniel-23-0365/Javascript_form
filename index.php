4236e8d2375da4b2cfac364407e4a80d6d08256e
dlongsod23-0365 <dlongsod23-0365@cca.edu.ph>
2025-02-17 18:34:44 +0800

    form

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	
 	<body style="background-color: black; color:#FFF;">
 		<h1> Student Record</h1>
-		<table id="student_table"></table>
+		<table id="student_table" border="1"></table>
 		<script> 
 			const studentTable = document.getElementById("student_table");
 		
@@ -17,6 +17,28 @@
 			
 			row1co11.innerHTML = "LAST NAME";
 			row1co12.innerHTML = "FIRST NAME";
+			
+			const row2 = studentTable.insertRow();
+			const row2co11 = row2.insertCell();
+			const row2co12 = row2.insertCell();
+			
+			const inputLastName = document.createElement("input");
+			inputLastName.type="text";
+			inputLastName.placeholder="Insert Last Name";
+			
+			row2co11.append(inputLastName);
+			
+			const inputFirstName = document.createElement("input");
+			inputFirstName.type="text";
+			inputFirstName.placeholder="Insert First Name";
+			
+			row2co12.append(inputFirstName);
+			
+			const row3 = studentTable.insertRow();
+			const row3co11 = row3.insertCell();
+			row3co11.colSpan="2";
+			row3co11.innerHTML ="TEST";
+			
 		</script>
 	</body>
 </html>
